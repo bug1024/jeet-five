02b65c1544de57a4c21b6e00296d3436b7243193
bug1024 <bug1024.com@gmail.com>
2017-05-20 12:35:14 +0800

    different language

--- ext/five/config.m4
+++ ext/five/config.m4
@@ -1,63 +1,10 @@
+dnl
 dnl $Id$
-dnl config.m4 for extension five
-
-dnl Comments in this file start with the string 'dnl'.
-dnl Remove where necessary. This file will not work
-dnl without editing.
-
-dnl If your extension references something external, use with:
-
-dnl PHP_ARG_WITH(five, for five support,
-dnl Make sure that the comment is aligned:
-dnl [  --with-five             Include five support])
-
-dnl Otherwise use enable:
+dnl
 
 PHP_ARG_ENABLE(five, whether to enable five support,
-dnl Make sure that the comment is aligned:
 [  --enable-five           Enable five support])
 
 if test "$PHP_FIVE" != "no"; then
-  dnl Write more examples of tests here...
-
-  dnl # --with-five -> check with-path
-  dnl SEARCH_PATH="/usr/local /usr"     # you might want to change this
-  dnl SEARCH_FOR="/include/five.h"  # you most likely want to change this
-  dnl if test -r $PHP_FIVE/$SEARCH_FOR; then # path given as parameter
-  dnl   FIVE_DIR=$PHP_FIVE
-  dnl else # search default path list
-  dnl   AC_MSG_CHECKING([for five files in default path])
-  dnl   for i in $SEARCH_PATH ; do
-  dnl     if test -r $i/$SEARCH_FOR; then
-  dnl       FIVE_DIR=$i
-  dnl       AC_MSG_RESULT(found in $i)
-  dnl     fi
-  dnl   done
-  dnl fi
-  dnl
-  dnl if test -z "$FIVE_DIR"; then
-  dnl   AC_MSG_RESULT([not found])
-  dnl   AC_MSG_ERROR([Please reinstall the five distribution])
-  dnl fi
-
-  dnl # --with-five -> add include path
-  dnl PHP_ADD_INCLUDE($FIVE_DIR/include)
-
-  dnl # --with-five -> check for lib and symbol presence
-  dnl LIBNAME=five # you may want to change this
-  dnl LIBSYMBOL=five # you most likely want to change this 
-
-  dnl PHP_CHECK_LIBRARY($LIBNAME,$LIBSYMBOL,
-  dnl [
-  dnl   PHP_ADD_LIBRARY_WITH_PATH($LIBNAME, $FIVE_DIR/$PHP_LIBDIR, FIVE_SHARED_LIBADD)
-  dnl   AC_DEFINE(HAVE_FIVELIB,1,[ ])
-  dnl ],[
-  dnl   AC_MSG_ERROR([wrong five lib version or lib not found])
-  dnl ],[
-  dnl   -L$FIVE_DIR/$PHP_LIBDIR -lm
-  dnl ])
-  dnl
-  dnl PHP_SUBST(FIVE_SHARED_LIBADD)
-
   PHP_NEW_EXTENSION(five, five.c, $ext_shared,, -DZEND_ENABLE_STATIC_TSRMLS_CACHE=1)
 fi
--- ext/five/five.c
+++ ext/five/five.c
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author:                                                              |
+  | Author: bug1024.com@gmail.com                                                              |
   +----------------------------------------------------------------------+
 */
 
@@ -26,6 +26,7 @@
 #include "php_ini.h"
 #include "ext/standard/info.h"
 #include "php_five.h"
+#include "../../Zend/zend_types.h"
 
 /* If you declare any globals in php_five.h uncomment this:
 ZEND_DECLARE_MODULE_GLOBALS(five)
@@ -34,23 +35,6 @@ ZEND_DECLARE_MODULE_GLOBALS(five)
 /* True global resources - no need for thread safety here */
 static int le_five;
 
-/* {{{ PHP_INI
- */
-/* Remove comments and fill if you need to have entries in php.ini
-PHP_INI_BEGIN()
-    STD_PHP_INI_ENTRY("five.global_value",      "42", PHP_INI_ALL, OnUpdateLong, global_value, zend_five_globals, five_globals)
-    STD_PHP_INI_ENTRY("five.global_string", "foobar", PHP_INI_ALL, OnUpdateString, global_string, zend_five_globals, five_globals)
-PHP_INI_END()
-*/
-/* }}} */
-
-/* Remove the following function when you have successfully modified config.m4
-   so that your module can be compiled into PHP, it exists only for testing
-   purposes. */
-
-/* Every user-visible function in PHP should document itself in the source */
-/* {{{ proto string confirm_five_compiled(string arg)
-   Return a string to confirm that the module is compiled in */
 PHP_FUNCTION(confirm_five_compiled)
 {
     char *arg = NULL;
@@ -65,56 +49,92 @@ PHP_FUNCTION(confirm_five_compiled)
 
     RETURN_STR(strg);
 }
-/* }}} */
-/* The previous line is meant for vim and emacs, so it can correctly fold and
-   unfold functions in source code. See the corresponding marks just before
-   function definition, where the functions purpose is also documented. Please
-   follow this convention for the convenience of others editing your code.
-*/
+
+
+// five function end
 
 PHP_FUNCTION(five)
 {
-    zend_string *strg;
-    strg = strpprintf(0, "five");
-    RETURN_STR(strg);
+    zend_long five;
+    five = 5;
+    RETURN_LONG(five);
 }
 
-/* {{{ php_five_init_globals
- */
-/* Uncomment this function if you have INI entries
-static void php_five_init_globals(zend_five_globals *five_globals)
+PHP_FUNCTION(five_english)
 {
-    five_globals->global_value = 0;
-    five_globals->global_string = NULL;
+    zend_string *str;
+    str = strpprintf(0, "five");
+    RETURN_STR(str);
 }
-*/
-/* }}} */
 
-/* {{{ PHP_MINIT_FUNCTION
- */
+PHP_FUNCTION(five_chinese)
+{
+    zend_string *str;
+    str = strpprintf(0, "五");
+    RETURN_STR(str);
+}
+
+PHP_FUNCTION(five_chinese_pinyin)
+{
+    zend_string *str;
+    str = strpprintf(0, "wǔ");
+    RETURN_STR(str);
+}
+
+PHP_FUNCTION(five_chinese_financial)
+{
+    zend_string *str;
+    str = strpprintf(0, "伍");
+    RETURN_STR(str);
+}
+
+PHP_FUNCTION(five_japanese)
+{
+    zend_string *str;
+    str = strpprintf(0, "五");
+    RETURN_STR(str);
+}
+
+PHP_FUNCTION(five_korean)
+{
+    zend_string *str;
+    str = strpprintf(0, "오");
+    RETURN_STR(str);
+}
+
+PHP_FUNCTION(five_binary)
+{
+    zend_long five;
+    five = 101;
+    RETURN_LONG(five);
+}
+
+PHP_FUNCTION(is_five)
+{
+    long number;
+
+    /* accepting arguments */
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &number) != SUCCESS) {
+        return;
+    }
+    zend_bool is_five;
+    is_five = number == 5;
+    RETURN_BOOL(is_five);
+}
+
+// five function end
+
+
 PHP_MINIT_FUNCTION(five)
 {
-    /* If you have INI entries, uncomment these lines
-    REGISTER_INI_ENTRIES();
-    */
     return SUCCESS;
 }
-/* }}} */
 
-/* {{{ PHP_MSHUTDOWN_FUNCTION
- */
 PHP_MSHUTDOWN_FUNCTION(five)
 {
-    /* uncomment this line if you have INI entries
-    UNREGISTER_INI_ENTRIES();
-    */
     return SUCCESS;
 }
-/* }}} */
 
-/* Remove if there's nothing to do at request start */
-/* {{{ PHP_RINIT_FUNCTION
- */
 PHP_RINIT_FUNCTION(five)
 {
 #if defined(COMPILE_DL_FIVE) && defined(ZTS)
@@ -122,44 +142,33 @@ PHP_RINIT_FUNCTION(five)
 #endif
     return SUCCESS;
 }
-/* }}} */
 
-/* Remove if there's nothing to do at request end */
-/* {{{ PHP_RSHUTDOWN_FUNCTION
- */
 PHP_RSHUTDOWN_FUNCTION(five)
 {
     return SUCCESS;
 }
-/* }}} */
 
-/* {{{ PHP_MINFO_FUNCTION
- */
 PHP_MINFO_FUNCTION(five)
 {
     php_info_print_table_start();
     php_info_print_table_header(2, "five support", "enabled");
     php_info_print_table_end();
-
-    /* Remove comments if you have entries in php.ini
-    DISPLAY_INI_ENTRIES();
-    */
 }
-/* }}} */
 
-/* {{{ five_functions[]
- *
- * Every user visible function must have an entry in five_functions[].
- */
 const zend_function_entry five_functions[] = {
     PHP_FE(five, NULL)
-    PHP_FE(confirm_five_compiled,   NULL)       /* For testing, remove later. */
-    PHP_FE_END  /* Must be the last line in five_functions[] */
+    PHP_FE(five_english, NULL)
+    PHP_FE(five_chinese, NULL)
+    PHP_FE(five_chinese_pinyin, NULL)
+    PHP_FE(five_chinese_financial, NULL)
+    PHP_FE(five_japanese, NULL)
+    PHP_FE(five_korean, NULL)
+    PHP_FE(five_binary, NULL)
+    PHP_FE(is_five, NULL)
+    PHP_FE(confirm_five_compiled, NULL)
+    PHP_FE_END
 };
-/* }}} */
 
-/* {{{ five_module_entry
- */
 zend_module_entry five_module_entry = {
     STANDARD_MODULE_HEADER,
     "five",
@@ -172,7 +181,6 @@ zend_module_entry five_module_entry = {
     PHP_FIVE_VERSION,
     STANDARD_MODULE_PROPERTIES
 };
-/* }}} */
 
 #ifdef COMPILE_DL_FIVE
 #ifdef ZTS
@@ -181,11 +189,3 @@ ZEND_TSRMLS_CACHE_DEFINE()
 ZEND_GET_MODULE(five)
 #endif
 
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * End:
- * vim600: noet sw=4 ts=4 fdm=marker
- * vim<600: noet sw=4 ts=4
- */
